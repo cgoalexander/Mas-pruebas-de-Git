9b7ec438551812bd004942303ad3c5e4d79d5ea6
Edinson Alexander Solano <cgoalexander731@gmail.com>
2022-08-12 10:16:23 -0500

    haciendo una prueba de varios repos

--- prueba.js
+++ prueba.js
@@ -1,3 +1,4 @@
 console.log ("Hola Mundo")
 console.log("Nuevo mensaje para probar control de versiones")
 console.log("nueva prueba de Git")
+console.log("haciendo un nuevo cambio desde otra rama")
